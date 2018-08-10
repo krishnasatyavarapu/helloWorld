17da83437ff835b2ac9d06c9a3228e2b0d9d9b1f
krishnasatyavarapu <ss.vamsikrishna@gmail.com>
2018-08-09 19:35:06 -0700

    First commit

---
+++ src/main/java/helloWorld.java
@@ -0,0 +1,9 @@
+import org.joda.time.LocalTime;
+public class helloWorld
+{
+    public static void main(String[] args)
+    {
+        LocalTime currTime = new LocalTime();
+        System.out.println("The current local time is" + currTime);
+    }
+}
