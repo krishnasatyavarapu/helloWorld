77509cba1f5bc594f4590f8ac6387ca2be8336c4
krishnasatyavarapu <ss.vamsikrishna@gmail.com>
2018-08-09 20:11:38 -0700

    second commit

--- src/main/java/helloWorld.java
+++ src/main/java/helloWorld.java
@@ -4,6 +4,7 @@ public class helloWorld
     public static void main(String[] args)
     {
         LocalTime currTime = new LocalTime();
+        System.out.print("THis is a new change");
         System.out.println("The current local time is" + currTime);
     }
 }
